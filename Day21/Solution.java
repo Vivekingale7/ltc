1c907b381ce5b8a5d6a7c064be74c5d9373b41f1
Viv___007 <vivekingale3989@gmail.com>
2024-03-15 19:08:39 +0530

    35_searchInsertPosition

--- Day21/Solution.java
+++
@@ -1,7 +0,0 @@
-import java.math.BigInteger;
-class Solution {
-     public static  String addBinary(String a, String b) {
-        BigInteger ss= new BigInteger(a,2).add(new BigInteger(b,2));
-       return ss.toString(2);
-    }
-}
